78cf69797e3f674e5a3ead30c7c4b1e1ac8956ed
William de Oliveira Sena <willsenaa@hotmail.com>
2025-08-06 10:44:35 -0300

    Update comandos.sh

--- comandos.sh
+++ comandos.sh
@@ -20,3 +20,23 @@ nano docker-compose.yml
 
 #Recria o conteiner
 docker compose up -d
+
+------------------
+ 
+#Comandos no shell do MySQL:
+#Acesse o Shell do conteiner
+Bash
+docker exec -it mysql-app bash
+
+#Acesse o Cliente do container MySQL
+docker exec -it mysql-app mysql -uroot -p
+
+#Lista os usuários criados no MySQL (localhost são usuários com permissão local / % são usuários permissões remotas)
+SELECT user, host FROM mysql.user;
+
+#Alterar senha
+SQL
+ALTER USER 'seu_user'@'%' IDENTIFIED BY 'SUA_NOVA_SENHA_FORTE';
+ALTER USER 'seu_user'@'localhost' IDENTIFIED BY 'SUA_NOVA_SENHA_FORTE';
+FLUSH PRIVILEGES;
+EXIT;
